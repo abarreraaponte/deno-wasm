c6e62d70680905199dff01750f59529c7e9a7e23
Alejandro Barrera Aponte <abarrera@entropy.cl>
2024-10-06 15:55:53 -0300

    Create LedgerApiTest.php

---
+++ tests/Feature/Api/LedgerApiTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Currency;
+use App\Models\Ledger;
+
+test('ledger can be created', function () {
+
+	$currency = Currency::factory()->create();
+
+	$ledger = Ledger::factory()->make(['currency_id' => $currency->id]);
+
+	$response = $this->postJson('/api/ledgers', $ledger->toArray());
+
+	$response->assertStatus(201);
+});
+
+test('Duplicate ledger can not be created', function () {
+	$currency = Currency::factory()->create();
+
+	$ledger1 = Ledger::factory()->create(['currency_id' => $currency->id]);
+
+	$ledger2 = Ledger::factory()->make(['currency_id' => $currency->id, 'name' => $ledger1->name]);
+
+	$response = $this->postJson('/api/ledgers', $ledger2->toArray());
+
+	$response->assertStatus(422);
+});
