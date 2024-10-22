804742989efc33c444dab7f5370e412160a2c861
Alejandro Barrera Aponte <abarrera@entropy.cl>
2024-10-22 09:03:22 -0300

    WIP Inertia / React

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,14 +1,20 @@
 <?php
 
+test('registration screen can be rendered', function () {
+    $response = $this->get('/register');
+
+    $response->assertStatus(200);
+});
+
 test('new users can register', function () {
     $response = $this->post('/register', [
-        'first_name' => 'Test',
-        'last_name' => 'User',
-        'email' => 'test@example.com',
+        'first_name' => $this->faker->firstName,
+		'last_name' => $this->faker->lastName,
+        'email' => $this->faker->unique()->safeEmail,
         'password' => 'password',
         'password_confirmation' => 'password',
     ]);
 
     $this->assertAuthenticated();
-    $response->assertNoContent();
+    $response->assertRedirect(route('dashboard', absolute: false));
 });
