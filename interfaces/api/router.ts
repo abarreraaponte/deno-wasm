5845241277334d92b360e8f714bc2acd80ad36b5
Alejandro Barrera Aponte <abarrera@entropy.cl>
2024-12-25 15:39:18 -0500

    WIP

--- interfaces/api/router.ts
+++ interfaces/api/router.ts
@@ -4,12 +4,16 @@ import LedgerRouter from './handlers/ledger_api_handler.ts';
 import AccountRouter from './handlers/account_api_handler.ts';
 import EntityModelRouter from './handlers/entity_model_api_handler.ts';
 import TransactionModelRouter from './handlers/transaction_model_api_handler.ts';
+import Oauth2Router from './handlers/oauth2_handler.ts';
 
 const app = new Hono();
 
 app.get('/health', (c: Context) => {
 	return c.json({ status: 'ok' });
 });
+
+
+app.route('/oauth2', Oauth2Router);
 
 app.route('/api/accounts', AccountRouter);
 app.route('/api/unit-types', UnitTypeRouter);
