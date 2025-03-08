7c231937ae10b2a044dbf049fe41006a9687512e
Alejandro Barrera Aponte <abarrera@entropy.cl>
2025-03-08 12:47:00 -0300

    Drizzle migrations working with deno compile

--- core/services/database/db.ts
+++ core/services/database/db.ts
@@ -1,6 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import * as schema from './schema.ts';
 import postgres from 'postgres';
+import { join } from "@std/path/join";
 
 const user = Deno.env.get('KL_PG_USER') || '';
 const password = Deno.env.get('KL_PG_PASSWORD') || '';
@@ -14,3 +16,13 @@ export const postgresUrl = `postgres://${user}:${password}@${host}:${port}/${dat
 const queryClient = postgres(postgresUrl, { max: max_connections });
 
 export const db = drizzle(queryClient, { schema: schema });
+
+export async function migrateDb() {
+	const migrationFolder = join(String(import.meta.dirname), "./migrations");
+	console.log(`Migrating database with migrations folder: ${migrationFolder}`);
+	await migrate(db, {
+		migrationsFolder: migrationFolder,
+		migrationsTable: 'migrations',
+		migrationsSchema: 'public',
+	});
+  }
