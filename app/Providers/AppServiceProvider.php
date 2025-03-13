af0a3f1055e98e0d579c7e7fdb07f02225948eee
Alejandro Barrera Aponte <abarrera@entropy.cl>
2025-03-13 19:05:24 -0300

    WIP Cognito

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,7 +19,12 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
+    {	
+		/**
+		 * Register the Cognito Socialite Provider
+		 */
+        Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
+			$event->extendSocialite('cognito', \SocialiteProviders\Cognito\Provider::class);
+		});
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Cache;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
 	$cached = Cache::get('test_cached_value');
     return view('welcome', ['cached' => $cached]);
+});
+
+Route::middleware(['auth'])->get('/restricted', function () {
+	dd('You are in the restricted area');
+});
+
+Route::get('/auth/cognito/redirect', function () {
+	return Socialite::driver('cognito')->redirect();
+});
+
+Route::get('/auth/cognito/callback', function () {
+	$user = Socialite::driver('cognito')->user();
+	return $user;
 });
